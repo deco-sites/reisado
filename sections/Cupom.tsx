5b7efe0a6c93698c330eaca082df945a5a8f5da3
Tarso Jabbes <tarsojabbes@gmail.com>
2024-05-15 20:44:29 -0300

    Fix coupom section

--- sections/Cupom.tsx
+++ sections/Cupom.tsx
@@ -1,17 +1,16 @@
 export interface Props {
-    codigo: string;
-    descricao: string;
-  }
-  
-  export default function Cupom({
-    codigo = "REISADO10",
-    descricao = "Desconto de 10% nos produtos da Reisado",
-  }: Props) {
-    return (
-      <div>
-        <div>Cupom: {codigo}</div>
-        <div>{descricao}</div>
-      </div>
-    );
-  }
-  
+  codigo: string;
+  descricao: string;
+}
+
+export default function Cupom({
+  codigo = "REISADO10",
+  descricao = "Desconto de 10% nos produtos da Reisado",
+}: Props) {
+  return (
+    <div className="bg-pink-200 w-72">
+      <p><b>Cupom: {codigo}</b></p>
+      <p>{descricao}</p>
+    </div>
+  );
+}
